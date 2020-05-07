1c05e3e73856220b62e4ee2fe6aab1899eba4e20
Vladimir Smagin <21h@blindage.org>
2020-05-07 17:50:38 +0700

    add error message to create

--- records.go
+++ records.go
@@ -264,7 +264,9 @@ func (d *HCloudDNS) CreateRecord(record HCloudRecord) (HCloudAnswerGetRecord, er
 		errorResultString := HCloudAnswerErrorString{}
 		err = json.Unmarshal([]byte(respBody), &errorResultString)
 		if err != nil {
-			return HCloudAnswerGetRecord{}, err
+			err_message := HCloudError{}
+			err_message.Message = string(respBody)
+			return HCloudAnswerGetRecord{Error: err_message}, err
 		}
 		errorResult.Error.Message = errorResultString.Error
 		errorResult.Error.Code = resp.StatusCode
